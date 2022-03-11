96ce14e50fae792df043e8b9ded51c4615d55a93
d0kur0 <dev.dokuro@gmail.com>
2022-03-11 03:34:24 +0300

    chore: implement base model functions

--- database/userModel.go
+++ database/userModel.go
@@ -1,1 +1,63 @@
 package database
+
+import (
+	"errors"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/d0kur0/cui-server/graph/model"
+	"github.com/d0kur0/cui-server/graph/scalars"
+	"gorm.io/gorm"
+)
+
+type UserModel struct{}
+
+func (u *UserModel) GetByToken(token string) (user *model.User) {
+	result := db.
+		Joins("join user_tokens on user_tokens.token = ?", token).
+		First(&user)
+
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil
+	}
+
+	return
+}
+
+func (u *UserModel) GetByEmail(email string) (user *model.User) {
+	result := db.Where(&model.User{Email: scalars.Email(email)}).First(&user)
+
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil
+	}
+
+	return
+}
+
+func (u *UserModel) GetByEmailAndPassword(email string, password string) (user *model.User) {
+	return
+}
+
+func (u *UserModel) Create(userForCreate model.User) (createdUser *model.User, err error) {
+	userWithEqualEmail := u.GetByEmail(string(userForCreate.Email))
+	if userWithEqualEmail != nil {
+		return nil, errors.New("email already in use")
+	}
+
+	hashOfPassword, err := bcrypt.GenerateFromPassword([]byte(userForCreate.Password), 14)
+	if err != nil {
+		log.Printf("error on UserModel.Create bcrypt.GenerateFromPassword; %s", err)
+		return nil, errors.New("internal error")
+	}
+
+	userForCreate.Password = string(hashOfPassword)
+
+	userCreateResult := db.Create(&userForCreate)
+	if userCreateResult.Error != nil {
+		log.Printf("error on SignUp db.Create(user); %s", err)
+		return nil, errors.New("internal error")
+	}
+
+	return &userForCreate, nil
+}
--- database/userTokenModel.go
+++ database/userTokenModel.go
@@ -1,1 +1,33 @@
 package database
+
+import (
+	"errors"
+	"log"
+	"time"
+
+	"github.com/d0kur0/cui-server/graph/model"
+	"github.com/segmentio/ksuid"
+)
+
+type UserTokenModel struct{}
+
+func (ut *UserTokenModel) Create(userID int) (createdToken *model.UserToken, err error) {
+	tokenValue, err := ksuid.NewRandom()
+	if err != nil {
+		log.Printf("error on UserTokenModel.Create ksuid.NewRandom; %s", err)
+		return nil, errors.New("internal error")
+	}
+
+	newToken := model.UserToken{
+		UserID:    userID,
+		Token:     tokenValue.String(),
+		CreatedAt: time.Now(),
+	}
+
+	result := db.Create(&newToken)
+	if result.Error != nil {
+		return nil, err
+	}
+
+	return &newToken, nil
+}
